1eeadcc10456cefc9323881b95b69598acf62411
gianlucas34 <gianlucas@Gians-MacBook-Pro.local>
2022-06-15 17:38:59 -0300

    client ajust

--- lib/core/client.dart
+++ lib/core/client.dart
@@ -2,21 +2,12 @@ import 'package:dio/dio.dart';
 import 'package:pokedex/core/http_client_interface.dart';
 
 class Client implements IHttpClient {
-  final Dio dio;
-
-  Client._(this.dio);
-
-  factory Client() {
-    final dio = Dio();
-
-    dio.options.baseUrl = 'https://gist.githubusercontent.com';
-
-    return Client._(dio);
-  }
+  final Dio client =
+      Dio(BaseOptions(baseUrl: 'https://gist.githubusercontent.com'));
 
   @override
   Future get(String url) async {
-    final response = await dio.get(url);
+    final response = await client.get(url);
 
     return response.data;
   }
